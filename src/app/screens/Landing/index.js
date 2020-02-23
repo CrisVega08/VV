0beb0fc2db7ede7b94c199eb57e2d7e830858326
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-02-23 00:07:20 -0500

    contact section on desktop

--- src/app/screens/Landing/index.js
+++ src/app/screens/Landing/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Navbar from '../../components/navbar';
+
 import Home from './sections/Home';
 import Nails from './sections/Nails';
 import Face from './sections/Face';
-
-import Navbar from '../../components/navbar';
+import Contact from './sections/Contact';
 
 function Landing () {
   return (
@@ -13,6 +14,7 @@ function Landing () {
       <Home />
       <Nails />
       <Face />
+      <Contact />
     </>
   )
 }
