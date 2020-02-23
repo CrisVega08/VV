9ff03f93389528ccb5467537f9502b976af16446
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-02-22 21:59:21 -0500

    nails section for destock working

---
+++ src/app/screens/Landing/sections/Nails/constants.js
@@ -0,0 +1,31 @@
+import serviceOne from './assets/Servicio_01_3x.png';
+import serviceTwo from './assets/Servicio_02_3x.png';
+import serviceThree from './assets/Servicio_03_3x.png';
+import serviceFour from './assets/Servicio_04_3x.png';
+
+const data = {
+    "properties": [
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 0,
+        "picture": serviceOne,
+    },
+    {
+        "_id": "593e9297ec4cca9c56bf61af",
+        "index": 1,
+        "picture": serviceTwo
+    },
+    {
+        "_id": "593e929773c71925e5d7c11c",
+        "index": 2,
+        "picture": serviceThree
+    },
+    {
+        "_id": "593e92973d4d15eedb129aee",
+        "index": 3,
+        "picture": serviceFour,
+    }
+  ]
+}
+
+export default data;
--- src/app/screens/Landing/sections/Nails/index.js
+++ src/app/screens/Landing/sections/Nails/index.js
@@ -1,37 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import cn from "classnames";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './styles.module.scss';
+import  data  from './constants';
 
 function Carousel() {
-  const [options, setOptions] = useState([{img: '', alt: '1'}, {img: '', alt: '2'}]);
-  const [currentOption, setCurrentOption] = useState(0);
+  const [options] = useState(data.properties);
+  let [currentOption, setCurrentOption] = useState(0);
 
-  const prevOption = () =>
-    setCurrentOption(currentOption-1)
+  const prevOption = useCallback(
+    () => setCurrentOption(currentOption-1),[currentOption]
+  );
 
-  const nextOption = () =>
-    setCurrentOption(currentOption++)
+  const nextOption = useCallback(
+    () => setCurrentOption(currentOption+1),[currentOption]
+  );
 
   return (
     <div className={styles.carousel} >
       <div className={styles.carouselActions}>
         <div>
-          <button 
+          <button className={styles.carouselBtn}
             onClick={prevOption} 
             disabled={currentOption === 0}
-          >Prev</button>
-          <button 
+          >
+            <FontAwesomeIcon icon={faAngleLeft} color="white" />
+          </button>
+          <button className={styles.carouselBtn}
             onClick={nextOption} 
             disabled={currentOption === options.length-1}
-          >Next</button>
+          ><FontAwesomeIcon icon={faAngleRight} color="white" /></button>
         </div>
-        <h3>Manicure & Pedicure</h3>
-        <p>Embellece  tus manos y pies, con nuestros diseños y estilo moderno utilizando técnicas tradicionales.</p>
-        <div className={styles.dotsContainer} >
-
+          <h3 className={`normal-title ${styles.carouselTitle}`}>Manicure & Pedicure</h3>
+          <p className={`normal-text ${styles.carouselMessage}`}>Embellece  tus manos y pies, con nuestros diseños y estilo moderno utilizando técnicas tradicionales.</p>
+          <div className={styles.dotsContainer}>
+            {options.length && options.map((option, index) => (
+              <span key={option._id} className={cn(styles.dots, {[styles.activeDot]: index === currentOption})} />
+            ))}
+          </div>
+        </div>
+      <div className={styles.seletedOption}>
+        <div className={`${styles.cardsSlider} ${styles[`activeSlide${currentOption}`]}`}>
+          <div className={styles.cardsSliderWrapper} style={{
+                  'transform': `translateX(-${currentOption*(100/options.length)}%)`
+                }}>
+            {options.length && options.map((option) => <img className={cn(styles.card,{[styles.selectedCard]: currentOption === option.index})} key={option._id} src={option.picture} alt={option._id} />)}
+          </div>
         </div>
       </div>
-      <img src={options[currentOption].img} alt={options[currentOption].alt} />
     </div>
   )
 }
