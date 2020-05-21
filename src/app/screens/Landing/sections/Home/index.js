caf8fc56d0c0ae5bc1fbbb86439ecc826f8ad7f4
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-05-21 11:10:23 -0500

    separe scroll icon

--- src/app/screens/Landing/sections/Home/index.js
+++ src/app/screens/Landing/sections/Home/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
-
-import scrollIcon from  './assets/mouse.png'
+import ScrollIcon from '../../../../components/scrollIcon';
 import { SERVICES } from './constants';
 import styles from './styles.module.scss';
 
 function Home() {
   return (
-    <section className={`slide ${styles.container}`} id="home">
-      <h3 className={`main-title ${styles.title}`}>
-        Cuidarte es la mejor elección
-      </h3>
-      <span className={`normal-text ${styles.wellcome}`}>
-        En <strong>Verónica Vega Nails & Spa</strong> te ofrecemos los mejores servicios con el mejor talento humano  y los mejores profesionales.
-      </span>
-      <div className={styles.services}>
-        {SERVICES.map(({id, name}) => (
-          <button className={styles.service} key={id} type="button">{name}</button>
-        ))}
-      </div>
-      <h4 className={`normal-title ${styles.invitation}`}>¡ Te esperamos para que vivas una experiencia de lujo !</h4>
-      <div className={styles.scrollElement}>
-        <img className={styles.scrollIcon} src={scrollIcon} alt="scroll-icon" />
-        <span className={styles.scrollText}>Scroll</span>
-      </div>
-    </section>
-  )
+    <>
+      <section className={`slide ${styles.container}`} id="home">
+        <h3 className={`main-title ${styles.title}`}>Cuidarte es la mejor elección</h3>
+        <span className={`normal-text ${styles.wellcome}`}>
+          En <strong>Verónica Vega Nails & Spa</strong> te ofrecemos los mejores servicios con el
+          mejor talento humano y los mejores profesionales.
+        </span>
+        <div className={styles.services}>
+          {SERVICES.map(({ id, name }) => (
+            <button className={styles.service} key={id} type="button">
+              {name}
+            </button>
+          ))}
+        </div>
+        <h4 className={`normal-title ${styles.invitation}`}>
+          ¡ Te esperamos para que vivas una experiencia de lujo !
+        </h4>
+      </section>
+      <ScrollIcon />
+    </>
+  );
 }
 
 export default Home;
