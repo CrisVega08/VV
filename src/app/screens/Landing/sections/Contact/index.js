e0c17699790fc66ff23cc0e8c5d0f3fdf5723e86
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2020-06-09 11:11:57 -0500

    add functions to icons

--- src/app/screens/Landing/sections/Contact/index.js
+++ src/app/screens/Landing/sections/Contact/index.js
@@ -3,10 +3,29 @@ import React from 'react';
 import ubication from './assets/shape.png';
 import phone from './assets/phone-call.png';
 import clock from './assets/wall-clock.png';
+import instagram from './assets/Instagram_simple_icon.svg';
 
 import styles from './styles.module.scss';
 
 function Contact() {
+  const generateMsnWhatsapp = () => {
+    const number = '{{ +573001234567 }}';
+    const message = 'Hola, me gustaría hacer una reserva'.split(' ').join('%20');
+    const url = `https://api.whatsapp.com/send?phone=${number}&text=%20${message}`;
+    window.open(url, '_blank'); //to open new page
+  };
+
+  const openInstagram = () => {
+    const url = 'https://www.instagram.com/veronicavega.nailsspa/';
+    window.open(url, '_blank');
+  };
+
+  const openMaps = () => {
+    //www.waze.com/livemap/directions?latlng=6.244376%2C-75.59171
+    const url = 'https://www.google.com/maps/@6.2444101,-75.591715,21z';
+    window.open(url, '_blank');
+  };
+
   return (
     <section className={`slide ${styles.container}`} id="contact">
       <h2 className={`main-title ${styles.title}`}>¿Qué esperas para visitarnos?</h2>
@@ -16,7 +35,7 @@ function Contact() {
       </span>
       <div className={styles.options}>
         <div className={styles.option}>
-          <img className={styles.iconOption} src={ubication} alt="ubication" />
+          <img className={styles.iconOption} onClick={openMaps} src={ubication} alt="ubication" />
           <h4 className="contact-option">Ubicación</h4>
           <span className="contact-info-option">Cl 39 D 74-01</span>
           <span className="normal-text">Laureles. Medellín</span>
@@ -30,11 +49,22 @@ function Contact() {
           <span className="normal-text">Sábados</span>
         </div>
         <div className={styles.option}>
-          <img className={styles.iconOption} src={phone} alt="phone" />
+          <img
+            className={styles.iconOption}
+            src={phone}
+            alt="phone"
+            onClick={generateMsnWhatsapp}
+          />
           <h4 className="contact-option">Número único de citas</h4>
           <span className="contact-info-option">(300) 123 45 67</span>
         </div>
       </div>
+      <img
+        className={styles.instagramIcon}
+        src={instagram}
+        alt="scroll-icon"
+        onClick={openInstagram}
+      />
     </section>
   );
 }
